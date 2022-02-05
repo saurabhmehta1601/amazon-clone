635acb358cd8b6bf43b03beed9aa0d2d1e6fc26a
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-06 00:33:59 +0530

    improve header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Image from "next/image";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      {/* Logo */}
       <Image
         className={styles.headerLogo}
         src="/logo.svg"
         alt="amazon-logo"
-        height="50"
-        width="80"
+        height="35"
+        width="35"
       />
+      {/* Search */}
       <div className={styles.headerSearch}>
         <input className={styles.headerSearchInput} type="text" />
+        <SearchSharpIcon className={styles.headerSearchIcon} />
       </div>
+      {/* Navigation */}
       <div className={styles.headerNav}>
         <div className={styles.headerOption}>
           <span className={styles.headerOptionLineOne}>Hello</span>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import { Header } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Roboto Font */}
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
