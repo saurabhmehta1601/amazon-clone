5d7f89bd55f8b43d41893fe5ae400e4c5aca08ff
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-06 09:46:49 +0530

    add hero img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import styles from "../styles/home.module.css";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="app">
-      <h1>Hello world</h1>;
+    <div className={styles.home}>
+      <div className={styles.homeContainer}>
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          className={styles.heroImg}
+          alt="img-banner"
+        />
+      </div>
     </div>
   );
 }
