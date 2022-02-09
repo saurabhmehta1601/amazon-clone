9f4021359aef78cd547f9c77e445baf3e52d9ad3
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-09 16:23:04 +0530

    update component exports in components/index.tsx file

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 export { default as Header } from "./Header";
+export { default as ThemeButton } from "./ThemeButton";
+export { default as SubTotal } from "./SubTotal";
+export { default as CartItem } from "./CartItem";
+export { default as Product } from "./Product";
