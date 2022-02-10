b67511674a1d15c726a0fa473aa2b359477d3497
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-10 20:33:02 +0530

    update form state on email or password change

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,42 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeButton } from "../components";
 import styles from "../styles/login.module.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
-      <div className={styles.layout}>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/263px-Amazon_logo.svg.png"
-          alt=""
-        />
-
-        <div className={styles.container}>
-          <h2>Sign In</h2>
-          <form className={styles.form}>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
-          </form>
-          <ThemeButton type="button" onClick={() => {}}>
-            Sign In
-          </ThemeButton>
-          <small>
-            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
-            Sale. Please see our Privacy Notice, our Cookies Notice and our
-            Interest-Based Ads Notice.
-          </small>
-          <ThemeButton
-            type="button"
-            onClick={() => {}}
-            style={{ backgroundColor: "#EFEFEF" }}
-          >
-            Create your Amazon account
-          </ThemeButton>
-        </div>
+    <div className={styles.layout}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/263px-Amazon_logo.svg.png"
+        alt=""
+      />
+      <div className={styles.container}>
+        <h2>Sign In</h2>
+        <form className={styles.form}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+        <ThemeButton type="button" onClick={() => {}}>
+          Sign In
+        </ThemeButton>
+        <small>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </small>
+        <ThemeButton
+          type="button"
+          onClick={() => {}}
+          style={{ backgroundColor: "#EFEFEF" }}
+        >
+          Create your Amazon account
+        </ThemeButton>
       </div>
-    </>
+    </div>
   );
 };
 
