0eed2237abb6204a109b842ee483d1f5ae79b2f3
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-10 20:24:46 +0530

    feat(ThemeButton): additional styles can be passed as prop

--- src/components/ThemeButton/index.tsx
+++ src/components/ThemeButton/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-const ThemeButton = ({ children, type, onClick }) => {
+interface IProps {
+  children: React.ReactChild;
+  type: "button" | "submit";
+  onClick: () => void;
+  style?: object;
+}
+
+const ThemeButton = ({ children, type, onClick, style }: IProps) => {
   return (
-    <button onClick={onClick} type={type} className={styles.btn}>
+    <button onClick={onClick} type={type} className={styles.btn} style={style}>
       {children}
     </button>
   );
