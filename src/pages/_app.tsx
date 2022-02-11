088a4f146e28dcb13afb7809b9d81a27d82080fa
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-11 17:28:56 +0530

    update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,22 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import UserProvider from "../components/UserProvider";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* Roboto Font */}
       <Provider store={store}>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <Component {...pageProps} />
+        <UserProvider>
+          <Head>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </UserProvider>
       </Provider>
     </>
   );
