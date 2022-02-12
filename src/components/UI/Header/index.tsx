bf0ce3a756e9c318780aefdb961e6af350b8c0cf
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-02-12 23:24:34 +0530

    add link to verify email in header

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -6,12 +6,15 @@ import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import { useRouter } from "next/router";
 import { logoutUser } from "../../../firebase/auth/utils";
 import { setUser } from "../../../redux/features/LoggedUser/userSlice";
+import { sendEmailVerification } from "firebase/auth";
+import { auth } from "../../../firebase/firebase";
 
 const Header = () => {
   const cart = useAppSelector((state) => state.cart);
   const user = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
   const router = useRouter();
+
   const navigateToCheckout = () => {
     router.push("/checkout");
   };
@@ -26,52 +29,73 @@ const Header = () => {
       dispatch(setUser(null));
     });
   };
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser)
+      .then(() => {
+        alert("Email for verification has been sent .");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
-    <div className={styles.header}>
-      {/* Logo */}
-      <Image
-        onClick={navigateToHome}
-        className={styles.headerLogo}
-        src="/logo.svg"
-        alt="amazon-logo"
-        height="35"
-        width="35"
-      />
-      {/* Search */}
-      <div className={styles.headerSearch}>
-        <input className={styles.headerSearchInput} type="text" />
-        <FaSearch className={styles.headerSearchIcon} />
+    <>
+      <div className={styles.header}>
+        {/* Logo */}
+        <Image
+          onClick={navigateToHome}
+          className={styles.headerLogo}
+          src="/logo.svg"
+          alt="amazon-logo"
+          height="35"
+          width="35"
+        />
+        {/* Search */}
+        <div className={styles.headerSearch}>
+          <input className={styles.headerSearchInput} type="text" />
+          <FaSearch className={styles.headerSearchIcon} />
+        </div>
+        {/* Navigation */}
+        <div className={styles.headerNav}>
+          <div className={styles.headerOption}>
+            <span className={styles.headerOptionLineOne}>
+              {user?.email ? user?.email : "Hello"}
+            </span>
+            <span className={styles.headerOptionLineTwo}>
+              {user?.email ? (
+                <span onClick={signOutUser}>Sign Out</span>
+              ) : (
+                <span onClick={navigateToLogin}>Sign In</span>
+              )}
+            </span>
+          </div>
+          <div className={styles.headerOption}>
+            <span className={styles.headerOptionLineOne}>Returns</span>
+            <span className={styles.headerOptionLineTwo}>& Orders</span>
+          </div>
+          <div className={styles.headerOption}>
+            <span className={styles.headerOptionLineOne}>Your</span>
+            <span className={styles.headerOptionLineTwo}>Prime</span>
+          </div>
+          <div
+            className={styles.headerOptionBasket}
+            onClick={navigateToCheckout}
+          >
+            <FaShoppingCart className={styles.headerBasketIcon} />
+            <span className={styles.headerBasketCount}>
+              {cart.products.length}
+            </span>
+          </div>
+        </div>
       </div>
-      {/* Navigation */}
-      <div className={styles.headerNav}>
-        <div className={styles.headerOption}>
-          <span className={styles.headerOptionLineOne}>
-            {user?.email ? user?.email : "Hello"}
-          </span>
-          <span className={styles.headerOptionLineTwo}>
-            {user?.email ? (
-              <span onClick={signOutUser}>Sign Out</span>
-            ) : (
-              <span onClick={navigateToLogin}>Sign In</span>
-            )}
-          </span>
+      {auth.currentUser && !auth.currentUser.emailVerified && (
+        <div className={styles.verifyEmail}>
+          Dear user your email is not verified . Click
+          <span onClick={verifyEmail}> here </span>
+          to get an email for account verification .
         </div>
-        <div className={styles.headerOption}>
-          <span className={styles.headerOptionLineOne}>Returns</span>
-          <span className={styles.headerOptionLineTwo}>& Orders</span>
-        </div>
-        <div className={styles.headerOption}>
-          <span className={styles.headerOptionLineOne}>Your</span>
-          <span className={styles.headerOptionLineTwo}>Prime</span>
-        </div>
-        <div className={styles.headerOptionBasket} onClick={navigateToCheckout}>
-          <FaShoppingCart className={styles.headerBasketIcon} />
-          <span className={styles.headerBasketCount}>
-            {cart.products.length}
-          </span>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
