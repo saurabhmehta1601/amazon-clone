aab6a11129d6d20d0ed9d4c59f4b7c82e1050f1f
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-03-21 11:34:42 +0530

    update UI for chekout for empty basket

--- src/pages/reviewCart.tsx
+++ src/pages/reviewCart.tsx
@@ -19,16 +19,28 @@ const Checkout = () => {
           <div className={styles.checkoutTitle}>
             <h2>Your Shopping Basket</h2>
             <div className={styles.cartItemsList}>
-              {cart.products.map((item) => (
-                <CartItem
-                  id={item.id}
-                  key={item.id}
-                  title={item.title}
-                  price={item.price}
-                  rating={item.rating}
-                  image_url={item.image_url}
-                />
-              ))}
+              {cart.products.length === 0 ? (
+                <div className={styles.empty_cart}>
+                  <a
+                    href="https://www.flaticon.com/free-icons/shopper"
+                    title="shopper icons"
+                  >
+                    <Image src="/empty-cart.png" width={200} height={200} />
+                  </a>
+                  <h3>Your cart is empty</h3>
+                </div>
+              ) : (
+                cart.products.map((item) => (
+                  <CartItem
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    price={item.price}
+                    rating={item.rating}
+                    image_url={item.image_url}
+                  />
+                ))
+              )}
             </div>
           </div>
         </div>
